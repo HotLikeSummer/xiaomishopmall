4e232c4c09ac51bdf1571905e409908d4dcab032
汤武 <1549968751@qq.com>
2019-11-02 11:44:32 +0800

    新增最新

---
+++ xiaomishopmall/pages/index/new/new.vue
@@ -0,0 +1,57 @@
+<template>
+	<view id="new">
+		<view class="title">
+			<!-- 循环导航栏内容 -->
+			<view v-for="(tab, index) in title" class="uni-tab-item" :key="index" :index="index" @click="ontabtap(index)">
+				<!-- 绑定样式 -->
+				<text class="uni-tab-item-title" :class="tabIndex == index ? 'uni-tab-item-title-active' : ''">{{tab}}</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				//导航下标
+				tabIndex:0,
+				//导航栏内容
+				title:["最新上新","最新上新","最新上新","最新上新"]
+			}
+		},
+		methods:{
+			//导航栏绑定样式
+			ontabtap(index) {
+				this.tabIndex = index
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.title {
+		width: 750upx;
+		height: 80upx;
+		display: flex;
+		justify-content: space-between;
+	}
+	
+	.uni-tab-item {
+		display: inline-block;
+		padding-left: 34upx;
+		padding-right: 34upx;
+	}
+	
+	.uni-tab-item-title {
+		color: #555;
+		font-size: 30upx;
+		height: 80upx;
+		line-height: 80upx;
+		padding: 20rpx 0rpx;
+	}
+	.uni-tab-item-title-active{
+		border-bottom: 5rpx solid #FD6801;
+		color: #FD6801 ;
+	}
+</style>
