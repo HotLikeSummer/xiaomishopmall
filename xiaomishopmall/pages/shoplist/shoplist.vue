ab5b3b5d54ebcfc4c9c46ea08594930c47133395
lly <ly1832001@163.com>
2019-11-14 19:34:10 +0800

    创建了新的文件

---
+++ xiaomishopmall/pages/shoplist/shoplist.vue
@@ -0,0 +1,29 @@
+<template>
+	<view id="shop-list">
+		<!-- 组件模板 -->
+		<Uni-Shop-List/>
+	</view>
+</template>
+
+<script>
+	// 引入组件模板
+	 import UniShopList from '@/components/uni-shoplist/uni-shoplist.vue'
+	export default{
+		// 注册组件
+		components:{UniShopList},
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	*{
+		margin: 0upx;
+		padding: 0upx;
+		color: #555555;
+	}
+	
+</style>
